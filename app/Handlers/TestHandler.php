a60e2cea9709e200d9fa4765f0a2cf166615308c
Bereczky Balazs <balazs.bereczky@stilldesign.hu>
2018-08-31 18:09:20 +0200

    added env as class

--- app/Handlers/TestHandler.php
+++ app/Handlers/TestHandler.php
@@ -2,14 +2,23 @@
 
 namespace App\Handlers;
 
+use App\Constants;
 use App\Contracts\Handlers\TestHandlerInterface;
+use Curl\Curl;
 
 class TestHandler implements TestHandlerInterface
 {
+    private $curl;
+
+    public function __construct(Curl $curl)
+    {
+        $this->curl = $curl;
+    }
+
     public function hello()
     {
-        $response = new \stdClass();
-        $response->message = 'hello world';
-        return $response;
+        $this->curl->setHeader('API_KEY', $_ENV['USERS_API_KEY']);
+        $this->curl->get('http://users.velopene.site/');
+        return json_decode($this->curl->response);
     }
 }
