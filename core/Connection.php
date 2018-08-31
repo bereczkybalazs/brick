aa9d70ba2b55da43902fbcc18e1cb7c1d09667d1
Bereczky Balazs <balazs.bereczky@stilldesign.hu>
2018-08-31 11:54:27 +0200

    dotenv fixes, htaccess fixes

--- core/Connection.php
+++ core/Connection.php
@@ -12,11 +12,11 @@ final class Connection
     {
         if (!self::$connection) {
             self::$connection = new PDO(
-                getenv('DB_CONNECTION') .
-                ':host=' . getenv('DB_HOST') . ':' . getenv('DB_PORT') .
-                ';dbname=' . getenv('DB_DATABASE'),
-                getenv('DB_USERNAME'),
-                getenv('DB_PASSWORD')
+                $_ENV['DB_CONNECTION'] .
+                ':host=' . $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT'] .
+                ';dbname=' . $_ENV['DB_DATABASE'],
+                $_ENV['DB_USERNAME'],
+                $_ENV['DB_PASSWORD']
             );
             self::$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
         }
